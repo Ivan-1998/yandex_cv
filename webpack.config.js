5105bdec368aff03ccd0a1798c9b9697201bacc0
Ivan <durnev@sibtiger.com>
2019-04-30 10:37:37 +0600

    remove eslint

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
             },
             {
                 test: /\.js$/,
-                loader: 'eslint-loader',
+                loader: 'babel-loader',
                 exclude: '/node_modules/'
             },
             {
